f2270b9452e26e9de8b1963c429b131e100ea7aa
HedgehogKU <ccc908925@gmail.com>
2020-05-16 16:32:35 +0800

    完成 problem PC 收合效果

--- app/assets/script/all.js
+++ app/assets/script/all.js
@@ -1,12 +1,23 @@
 const icon = document.querySelector('.ham_icon');
 const dropdown = document.querySelector('.dropdown');
 const iconStyle = document.querySelector('#icon');
+const problemList = document.querySelector('.problem_content .list')
 
 icon.addEventListener('click', toggle);
+problemList.addEventListener('click', toggleList);
 
 function toggle(e) {
   e.preventDefault();
   dropdown.classList.toggle('active');
   iconStyle.classList.toggle('icon-menu');
   iconStyle.classList.toggle('icon-cross');
+}
+
+function toggleList(e) {
+  if (e.target.nodeName !== 'H4') {
+    return;
+  }
+  e.target.parentNode.classList.toggle('active');
+  e.target.nextElementSibling.classList.toggle('fadeInDown')
+  e.target.nextElementSibling.classList.toggle('am-order-1')
 }
